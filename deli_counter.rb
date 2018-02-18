9205691cfceaf7f07433f0c2acdedf4424961a1a
Reno Sorensen <renojames19@gmail.com>
2018-02-18 23:16:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+
+
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    deli_line = []
+    katz_deli.each_with_index do |name, i|
+      deli_line.push("#{i + 1}. #{name}")
+    end
+    deli_line = deli_line.join(" ")
+    puts "The line is currently: #{deli_line}"
+  end
+end
